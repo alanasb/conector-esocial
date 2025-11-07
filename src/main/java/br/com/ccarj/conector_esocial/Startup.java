0ebae435f7bfda6cf4b9d2035a38b0e2aec1f57e
alanasb <lanbaptista@gmail.com>
2025-11-07 00:15:52 -0300

    Arquivos iniciais do projeto

---
+++ src/main/java/br/com/ccarj/conector_esocial/Startup.java
@@ -0,0 +1,13 @@
+package br.com.ccarj.conector_esocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConectorEsocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConectorEsocialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ccarj/conector_esocial/ConectorEsocialApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ccarj.conector_esocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConectorEsocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
